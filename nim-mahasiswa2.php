735ce9d23af6a7a25bbe866523668bcd524747c7
Levimuhammad <muhammad.asshidiqi@mhs.unsoed.ac.id>
2024-05-30 14:29:56 +0700

    Update nim-mahasiswa2.php
    coba-coba

--- nim-mahasiswa2.php
+++ nim-mahasiswa2.php
@@ -1,1 +1,15 @@
-menambahkan file nim-mahasiswa2.php(Levi)
+<!DOCTYPE html>
+<html>
+<head>
+    <title>cobacoba</title>
+</head>
+<body>
+
+<?php
+    $pesan = "Halo, dunia!";
+    
+    echo "<h1>$pesan</h1>";
+?>
+
+</body>
+</html>
