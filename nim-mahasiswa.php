4663ecd5f502d12dabff37b7719b02bbb938d28a
raghnat <151110276+raghnat@users.noreply.github.com>
2024-06-01 01:58:00 +0700

    Update nim-mahasiswa.php

--- nim-mahasiswa.php
+++ nim-mahasiswa.php
@@ -4,15 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Belajar github </title>
 </head>
 
 <body>
-    <p>Percobaan Github</p>
-    <div>
-        <p>Test</p>
-        <p>Test</p>
-    </div>
 </body>
 
 </html>
